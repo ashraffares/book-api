7c18b7f60df62cdc1c3d4e5eff6f181c5b3c142b
ashraffares <ashraffares090@gmail.com>
2021-10-20 09:33:37 +0200

    Add session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#login'
       post '/registeration', to: 'sessions#registeration'
     end
   end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
