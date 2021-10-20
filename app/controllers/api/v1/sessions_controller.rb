cd6e278c66e21d17c0470c265a0190261322f0f2
ashraffares <ashraffares090@gmail.com>
2021-10-20 09:45:11 +0200

    Define login and register methods

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,21 @@
 class Api::V1::SessionsController < ApplicationController
   def login
+    user = User.find(email: params[:email])
+    if user&.authenticate(params[:password])
+      token = encrypt({ user_id: user.id })
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def registeration
+    new_user = User.create(email: params[:email], password: params[:password], password_confimration: params[:password_confimration])
+    if new_user.save
+      token = encrypt({ user_id: new_user.id })
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: new_user.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 end
