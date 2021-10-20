28200c43f5ae82dcb01dde7371e38a75e9647cff
ashraffares <ashraffares090@gmail.com>
2021-10-20 15:37:33 +0200

    Add more validation to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,5 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :messages
   validates :title, :author, :pagesTotal, :category, :user_id, presence: true
+  validates :pagesTotal, length: { maximum: 10_000, minimum: 2 }, numericality: { only_integer: true }
 end
