3d93b55e5b86c25cede98ce3e2e55fa144f5d148
ashraffares <ashraffares090@gmail.com>
2021-10-20 14:04:10 +0200

    Create erros controller to handle 404 and 500

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  match '/404', to: 'errors#not_found', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#login'
