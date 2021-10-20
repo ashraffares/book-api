51388716df37e4ddcfd592b18938c32565e2857f
ashraffares <ashraffares090@gmail.com>
2021-10-20 13:36:38 +0200

    update routes fil

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :messages
+
   namespace :api do
     namespace :v1 do
       post '/login', to: 'sessions#login'
       post '/registration', to: 'sessions#create'
-      resources :books
+      resources :books do 
+        resources :messages
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
